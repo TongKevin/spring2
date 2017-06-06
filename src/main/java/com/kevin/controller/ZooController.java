5bc0d9941caba3085471e29d22b42a1534d512ca
Kevin.Tong <tmtongming@163.com>
2017-06-06 16:24:31 +0800

    json

--- src/main/java/com/kevin/controller/ZooController.java
+++ src/main/java/com/kevin/controller/ZooController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kevin.service.ZooManager;
 
@@ -17,9 +18,11 @@ public class ZooController {
 	private ZooManager zooManager;
 	
 	@RequestMapping(value="/eat/{id}", method=RequestMethod.GET)
-	public void zet(@PathVariable("id") String food)
+	@ResponseBody
+	public String zet(@PathVariable("id") String food)
 	{
 		System.out.println("food="+food);
 		zooManager.eat(food);
+		return "{'a':'"+food+"'}";
 	}
 }
